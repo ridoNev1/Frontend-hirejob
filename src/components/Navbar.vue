6e55d984394d2bba479ad02480831761b420fdf4
ridoNev1 <ridolaurent021123@gmail.com>
2020-10-10 11:24:14 +0700

    update navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,19 +1,14 @@
 <template>
-  <div class="navbar-box bg-white">
-  <nav class="navbar navbar-expand-lg navbar-light bg-white container">
-    <a class="navbar-brand" href="#"><img src="../assets/img/exam.svg" alt="logo"></a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-        <a class="nav-link" href="#">Features</a>
-        <a class="nav-link" href="#">Pricing</a>
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+  <div class="navbar-box">
+    <div class="navbar-page container">
+      <img src="../assets/img/exam.svg" alt="logo">
+      <div class="login-regbtn">
+        <div class="need-login">
+          <button class="btn">Daftar</button>
+          <button class="btn">Masuk</button>
+        </div>
       </div>
     </div>
-  </nav>
   </div>
 </template>
 
@@ -22,3 +17,51 @@ export default {
   name: 'Navbar'
 }
 </script>
+
+<style scoped>
+.navbar-page {
+  padding: 15px;
+}
+.navbar-page {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.navbar-page img {
+  width: 127px;
+  height: 35px;
+}
+
+.need-login button {
+  font-weight: bold;
+}
+.need-login button:nth-child(1) {
+  border: 2px solid #5E50A1;
+  color: #5E50A1;
+}
+.need-login button:nth-child(1):hover {
+  background-color: #5E50A1;
+  color: white;
+}
+.need-login button:nth-child(2) {
+  background-color: #5E50A1;
+  color: white;
+  border: 2px solid #5E50A1;
+  margin-left: 15px;
+}
+.need-login button:nth-child(2):hover {
+  background-color: transparent;
+  color: #5E50A1;
+}
+
+@media screen and (max-width: 567px) {
+  .navbar-page {
+    position: fixed;
+    bottom: 0;
+    background-color: #fff;
+    z-index: 2;
+    box-shadow: 0px -6px 20px rgba(175, 175, 175, 0.25);
+    border-radius: 30px 30px 0px 0px;
+  }
+}
+</style>
