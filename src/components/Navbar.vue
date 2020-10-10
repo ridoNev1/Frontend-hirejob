152cfb51f45462fa7f47b624add6f178a2ff1b4f
sofyan-dev <mchsfyan17@gmail.com>
2020-10-10 10:46:42 +0700

    update navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="navbar-box">
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="navbar-box bg-white">
+  <nav class="navbar navbar-expand-lg navbar-light bg-white">
     <a class="navbar-brand" href="#"><img src="../assets/img/exam.svg" alt="logo"></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
