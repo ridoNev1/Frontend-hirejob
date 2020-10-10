a7e728032029d1dd337d7ab5a99aa028b8c3fa37
sofyan-dev <mchsfyan17@gmail.com>
2020-10-10 15:17:55 +0700

    responsive home

--- src/components/CardHome.vue
+++ src/components/CardHome.vue
@@ -1,16 +1,19 @@
 <template>
     <div>
         <div class="my-2">
-  <b-card class="card-home ">
-      <img style="width:100px; height:100px" src="../assets/img/58997508.jpg" alt="">
+  <b-card class="card-home">
+    <div class="img-card">
+      <span class="img"></span>
+    </div>
     <b-card-text class="mt-3">
+      <h3>Riko Arafi</h3>
         <p>Fullstack Developer</p>
           <img src="../assets/img/map-pin (4) 1.svg" alt="">
         Jakarta Timur
     </b-card-text>
     <div class="skill d-flex justify-content-start" >
     <button class="btn text-white  mx-2">PHP</button>
-    <button class="btn text-white  mx-2">JAVASCRIPT</button>
+    <button class="btn text-white  mx-2">Javascript</button>
     </div>
   </b-card>
 </div>
@@ -23,8 +26,23 @@ border: 1px solid #FBB017;
 box-sizing: border-box;
 border-radius: 4px;
 }
+.skill{
+  overflow-x:hidden;
+}
 .card-home{
     background: #FFFFFF;
 border-radius: 4px;
+padding-left: 4px;
+}
+.img-card{
+  width: 100px;
+  height: 100px;
+  background-image: url('../assets/img/card-img.jpg');
+  background-size: cover;
+}
+@media only screen and (max-width: 600px) {
+  .card-home {
+    text-align: left;
+  }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,47 +8,50 @@
         <div>
         <b-col lg="12">
             <b-row >
-                <b-col lg="12" class="my-5 px-5">
-                    <div class="input-group px-5 text-center">
-                        <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username with two button addons" aria-describedby="button-addon4">
-                        <div class="input-group-append" id="button-addon4">
-                            <button class="btn btn-outline-secondary" type="button">Button</button>
-                            <button class="btn btn-outline-secondary" type="button">Button</button>
-                        </div>
-                        </div>
+                <b-col lg="12" class="my-5">
+                    <div class="search">
+                        <form class="form">
+                            <input type="text" placeholder="Search any skill">
+                            <div style="margin-top: -2px;">
+                                <b-dropdown  id="dropdown-left" text="Category" variant="transparent" class="m-2">
+                                    <b-dropdown-item href="#">Action</b-dropdown-item>
+                                    <b-dropdown-item href="#">Another action</b-dropdown-item>
+                                    <b-dropdown-item href="#">Something else here</b-dropdown-item>
+                                </b-dropdown>
+                            </div>
+                            <button type="submit" class="btn searchButton">Search</button>
+                        </form>
+                    </div>
                 </b-col>
-                <b-col lg="12" class="my-3 mb-4 px-5">
-                    <b-row class="px-5">
-                        <b-col lg="4">
+                <b-col lg="12" class="my-3 mb-4">
+                    <b-row class="card-body">
+                        <b-col lg="3" sm="6" cols="6">
                             <CardHome />
                         </b-col>
-                        <b-col lg="4">
+                        <b-col lg="3" sm="6" cols="6">
                             <CardHome />
                         </b-col>
-                        <b-col lg="4">
+                        <b-col lg="3" sm="6" cols="6">
                             <CardHome />
                         </b-col>
-                        <b-col lg="4">
+                        <b-col lg="3" sm="6" cols="6">
                             <CardHome />
                         </b-col>
-                        <b-col lg="4">
-                            <CardHome />
-                        </b-col>
-                        <b-col lg="4">
+                        <b-col lg="3" sm="6" cols="6">
                             <CardHome />
                         </b-col>
                     </b-row>
                 </b-col>
-                <b-col lg="12" class="text-center px-5">
+                <b-col lg="12" class="pagination-component">
                     <nav aria-label="Page navigation example">
-  <ul class="pagination">
-    <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-    <li class="page-item"><a class="page-link" href="#">1</a></li>
-    <li class="page-item"><a class="page-link" href="#">2</a></li>
-    <li class="page-item"><a class="page-link" href="#">3</a></li>
-    <li class="page-item"><a class="page-link" href="#">Next</a></li>
-  </ul>
-</nav>
+                        <ul class="pagination">
+                            <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+                            <li class="page-item"><a class="page-link" href="#">1</a></li>
+                            <li class="page-item"><a class="page-link" href="#">2</a></li>
+                            <li class="page-item"><a class="page-link" href="#">3</a></li>
+                            <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                        </ul>
+                    </nav>
                 </b-col>
             </b-row>
         </b-col>
@@ -73,13 +76,68 @@ export default {
 .container-fluid{
     background: #F6F7F8;
 }
+.searchButton{
+    background-color: #5E50A1;
+    font-weight: bold;
+    border: 2px solid #5E50A1;
+    color: white;
+}
+.searchButton:hover{
+    background-color: white;
+    border: 2px solid #5E50A1;
+    color: #5E50A1;
+}
+.input-group{
+    padding: 15px;
+}
+.search input[type="text"]{
+    outline: none;
+    border: none;
+    border-right: 1px solid #9EA0A5;
+    padding: 0 15px;
+}
+.card-body{
+    padding: 0 5px ;
+}
+.search{
+    background-color: white;
+    padding: 5px;
+    box-shadow: 0px 1px 20px rgba(197, 197, 197, 0.25);
+border-radius: 8px;
+}
+.form{
+    display: grid;
+    grid-template-columns: 8fr 1fr 1fr;
+    grid-template-rows: 50px;
+}
+.sorting{
+    background-color: white !important;
+}
 .input-group .form-control{
-    height: 60px;
+    height: 50px;
     background: #FFFFFF;
 box-shadow: 0px 1px 20px rgba(197, 197, 197, 0.25);
 border-radius: 8px;
 }
+
+/* .dropdown{
+     height: 60px;
+    background: #FFFFFF;
+box-shadow: 0px 1px 20px rgba(197, 197, 197, 0.25);
+border-radius: 8px;
+} */
+.dropdown .btn{
+    height: 60px;
+}
 .pagination{
     text-align: center;
+}
+.pagination-component{
+    padding-left: 40%;
+}
+@media only screen and (max-width: 600px) {
+  .pagination-component {
+    padding: 0;
+  }
 }
 </style>
