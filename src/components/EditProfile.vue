2ec082a3dee69c9c4422be17d532940b40639819
sofyan-dev <mchsfyan17@gmail.com>
2020-10-11 08:38:31 +0700

    add profile company

---
+++ src/components/EditProfile.vue
@@ -0,0 +1,299 @@
+<template>
+    <div>
+        <div v-if="this.role === 0">
+        <!-- form pencari kerja -->
+        <b-row class="users">
+            <b-col lg="12" class="profile-user">
+                <b-row class="px-5">
+                    <h1 class="px-2 py-3">Data Diri</h1>
+                    <div class="line py-2"></div>
+                    <b-col lg="12">
+                        <form>
+                            <div class="form-group">
+                                <label>Nama Lengkap</label>
+                                <input type="text" class="form-control" placeholder="Masukan nama lengkap">
+                            </div>
+                            <div class="form-group">
+                                <label>Jobdesk</label>
+                                 <input type="text" class="form-control" placeholder="Masukan Jobdesk">
+                            </div>
+                            <div class="form-group">
+                                <label>Domisili</label>
+                                 <input type="text" class="form-control" placeholder="Masukan Domisili">
+                            </div>
+                            <div class="form-group">
+                                <label>Tempat Kerja</label>
+                                 <input type="text" class="form-control" placeholder="Masukan Tempat Kerja">
+                            </div>
+                            <div class="form-group">
+                                <label>Deskripsi singkat</label>
+                                <textarea class="form-control" rows="5" placeholder="Tuliskan deskripsi singkat"></textarea>
+                            </div>
+                        </form>
+                    </b-col>
+                </b-row>
+            </b-col>
+            <!-- skil -->
+            <b-col lg="12" class="skill-search my-4">
+                <b-row class="input-search px-5">
+                    <h1 class="px-2 py-3">Skill</h1>
+                    <b-col lg="12">
+                        <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="Golang">
+                        <button class="btn search-btn mx-3">Search</button>
+                        </div>
+                    </b-col>
+                </b-row>
+            </b-col>
+
+            <!-- experience -->
+            <b-col lg="12" class="experience my-4">
+                <b-row class="px-5">
+                    <h1 class="px-2 py-3">Pengalaman Kerja</h1>
+                    <div class="line py-2"></div>
+                    <b-col lg="12">
+                        <form>
+                            <div class="form-group">
+                                <label>Posisi</label>
+                                <input type="text" class="form-control" placeholder="Fullstack Developer">
+                            </div>
+                        </form>
+                    </b-col>
+                    <b-col lg="12">
+                                <b-row>
+                                    <b-col lg="6" class="text-left">
+                                        <div class="form-group">
+                                            <label>Nama Perusahaan</label>
+                                            <input type="text" class="form-control" placeholder="PT Apa Aja Boleh">
+                                        </div>
+                                    </b-col>
+                                    <b-col lg="6">
+                                        <div class="form-group">
+                                            <label class="text-left">Bulan/Tahun</label>
+                                            <input type="text" class="form-control" placeholder="Januari 2088">
+                                        </div>
+                                    </b-col>
+                                </b-row>
+                    </b-col>
+                    <b-col lg="12">
+                        <div class="form-group">
+                                <label>Deskripsi singkat</label>
+                                <textarea class="form-control" rows="5" placeholder="Deskripsi Pekerjaan anda"></textarea>
+                        </div>
+                    </b-col>
+                    <b-col lg="12" class="my-3">
+                        <button class="btn btn-experience">Tambah Pengalaman</button>
+                    </b-col>
+                </b-row>
+            </b-col>
+            <!-- end experience -->
+
+            <!-- start portofolio -->
+            <b-col lg="12" class="portofolio my-4">
+                <b-row class="px-5">
+                    <h1 class="px-2 py-3">Portofolio</h1>
+                    <div class="line py-2"></div>
+                    <b-col lg="12">
+                        <form>
+                            <div class="form-group">
+                                <label>Nama Aplikasi</label>
+                                <input type="text" class="form-control" placeholder="Masukkan nama aplikasi">
+                            </div>
+                            <div class="form-group">
+                                <label>Link Repository</label>
+                                <input type="text" class="form-control" placeholder="Masukkan link repository">
+                            </div>
+                            <div class="form-group">
+                                <label>Type Portfolio</label>
+                                <b-row>
+                                <b-col lg="6" sm="6" cols="6" class="radio">
+                                    <div class="radio-box text-left  py-2">
+                                        <div class="form-check">
+                                        <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+                                        <label class="form-check-label" for="exampleRadios1">
+                                            Aplikasi Web
+                                        </label>
+                                        </div>
+                                    </div>
+                                </b-col>
+                                <b-col lg="6" sm="6" cols="6" class="radio">
+                                    <div class="radio-box text-left  py-2">
+                                        <div class="form-check">
+                                        <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+                                        <label class="form-check-label" for="exampleRadios1">
+                                            Aplikasi Mobile
+                                        </label>
+                                        </div>
+                                    </div>
+                                </b-col>
+                            </b-row>
+                            </div>
+                            <div class="form-group">
+                                <label>Upload Gambar</label>
+                                <input type="file" class="form-control" >
+                            </div>
+                        </form>
+                    </b-col>
+                    <b-col lg="12" class="my-3">
+                        <button class="btn btn-experience">Tambah Portfolio</button>
+                    </b-col>
+                </b-row>
+            </b-col>
+            <!-- end portofolio -->
+        </b-row>
+        <!-- end form pencari kerja -->
+        </div>
+
+        <div v-else>
+        <!-- company / perekrut -->
+        <b-row >
+            <b-col lg="12" class="profile-company">
+                <b-row class="">
+                    <h1 class="px-2 py-3">Data Diri</h1>
+                    <div class="line py-2"></div>
+                    <b-col lg="12">
+                        <form>
+                            <div class="form-group">
+                                <label>Nama Perusahaan</label>
+                                <input type="text" class="form-control" placeholder="Masukan nama Perusahaan">
+                            </div>
+                            <div class="form-group">
+                                <label>Bidang</label>
+                                 <input type="text" class="form-control" placeholder="Masukan bidang perusahaan ex : Financial">
+                            </div>
+                            <div class="form-group">
+                                <label>Kota</label>
+                                 <input type="text" class="form-control" placeholder="Masukan Kota">
+                            </div>
+                            <div class="form-group">
+                                <label>Deskripsi singkat</label>
+                                <textarea class="form-control" rows="5" placeholder="Tuliskan deskripsi singkat"></textarea>
+                            </div>
+                            <div class="form-group">
+                                <label>Email</label>
+                                 <input type="text" class="form-control" placeholder="Masukan Email">
+                            </div>
+                            <div class="form-group">
+                                <label>Instagram</label>
+                                 <input type="text" class="form-control" placeholder="Masukan Nama Instagram">
+                            </div>
+                            <div class="form-group">
+                                <label>Nomor Telepon</label>
+                                 <input type="text" class="form-control" placeholder="Masukan Nomor Telepon">
+                            </div>
+                            <div class="form-group">
+                                <label>Linkedin</label>
+                                 <input type="text" class="form-control" placeholder="Masukan Nama Linkedin">
+                            </div>
+                        </form>
+                    </b-col>
+                </b-row>
+            </b-col>
+        </b-row>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      role: 1
+    }
+  }
+}
+</script>
+<style scoped>
+.profile-user{
+    background: #FFFFFF;
+border-radius: 8px;
+}
+.skill-search{
+    background: #FFFFFF;
+border-radius: 8px;
+}
+.experience{
+    background: #FFFFFF;
+border-radius: 8px;
+}
+.input-search .form-control{
+    height: 55px;
+}
+.search-btn{
+    background: #FBB017;
+    border-radius: 4px;
+    color: white;
+    border: 2px solid  #FBB017;
+}
+.search-btn:hover{
+    background: white;
+    border-radius: 4px;
+    color: #FBB017;
+    border: 2px solid  #FBB017;
+}
+.line{
+   border-bottom: 1px solid #C4C4C4;
+}
+.form-group label{
+    color: #9EA0A5;
+    font-size: 13px;
+}
+.form-control{
+    background: #FFFFFF;
+border: 1px solid #E2E5ED;
+box-sizing: border-box;
+border-radius: 4px;
+}
+
+/* experience */
+.btn-experience{
+height: 60px;
+width: 100%;
+border: 2px solid #FBB017;
+box-sizing: border-box;
+border-radius: 4px;
+color: #FBB017;
+}
+.btn-experience:hover{
+height: 60px;
+width: 100%;
+border: 2px solid #FBB017;
+box-sizing: border-box;
+border-radius: 4px;
+background-color:  #FBB017;
+color: white;
+}
+
+/* portofolio */
+.portofolio{
+    background: #FFFFFF;
+border-radius: 8px;
+}
+.radio-box{
+    height: 40px;
+    background: #FFFFFF;
+border: 1px solid #E2E5ED;
+box-sizing: border-box;
+border-radius: 4px;
+padding: 5px;
+}
+.radio:hover{
+    cursor: pointer;
+}
+
+/* company */
+.profile-company{
+    padding: 0px 45px;
+    background: #FFFFFF;
+border-radius: 8px;
+}
+@media only screen and (max-width: 600px) {
+  .radio-box {
+    padding: 1px;
+  }
+  .profile-company{
+    padding: 6px;
+    background: #FFFFFF;
+border-radius: 8px;
+}
+}
+</style>
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -3,178 +3,38 @@
         <Navbar />
 <!-- <div class="top-bg fluid"> -->
             <div class="container-fluid">
-            <b-col lg="12" class="profile px-5 py-5">
-                    <b-row class="">
-                    <b-col lg="4" class="pl-5">
+            <b-col lg="12" class="profile py-5">
+                    <b-row class="content mx-5">
+                    <b-col lg="4" cols="12" class="user-card">
                         <UserCard />
                     </b-col>
-                    <b-col lg="8" class="pr-5">
-                        <b-row>
-                            <b-col lg="12">
-                        <div class="profile-user">
-                        <h1 style="font-size: 25px;" class=" px-5 pt-4 pb-2">Data Diri</h1>
-                        <span class="data-diri mb-3">
-                            <img src="../assets/img/Line 10.svg" alt="">
-                        </span>
-                        <div class="px-5">
-                            <form>
-                            <div class="form-group">
-                                <label>Nama Lengkap</label>
-                                <input type="text" class="form-control" >
-                            </div>
-                            <div class="form-group">
-                                <label>Jobdesk</label>
-                                <input type="text" class="form-control">
-                            </div>
-                            <div class="form-group">
-                                <label >Domisili</label>
-                                <input type="text" class="form-control" >
-                            </div>
-                            <div class="form-group">
-                                <label >Tempat Kerja</label>
-                                <input type="text" class="form-control" >
-                            </div>
-                            <div class="form-group description">
-                                <label >Deskripsi Singkat</label>
-                                <input type="text" class="form-control" >
-                            </div>
-                            <button type="submit" class="btn btn-primary">Submit</button>
-                            </form>
-                        </div>
-                        </div>
-                        </b-col>
-                        <!-- skill -->
-                        <b-col lg="12" class="my-5 px-4">
-                            <b-row class="skill-profile px-5">
-                                <h1 style="font-size:25px" class="pt-4">Skill</h1>
-                                    <span class="line">
-                                <img src="../assets/img/Line 10.svg" alt="">
-                            </span>
-                                <b-col lg="12">
-                                    <div class="input-group my-4">
-                                    <input type="text" class="mx-2 form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
-                                    <!-- <div class="input-group-append"> -->
-                                    <button class="btn btn-save" type="button">
-                                        Simpan
-                                    </button>
-                                <!-- </div> -->
-                                </div>
-                            </b-col>
-                            </b-row>
-                        </b-col>
-                        <!-- pengalaman kerja  -->
-                        <b-col lg="12" class="my-5 px-4">
-                            <b-row class="experience">
-                                <h1 class="px-5 pt-4" style="font-size:25px;">Pengalaman Kerja</h1>
-                                <span>
-                                    <img src="../assets/img/Line 10.svg" alt="">
-                                </span>
-                                <b-col lg="12">
-                                <div class="px-5 py-2">
-                                    <form>
-                                        <div class="form-group">
-                                            <label>Posisi</label>
-                                            <input type="text" class="form-control" >
-                                        </div>
-                                        <b-row>
-                                            <b-col lg="6">
-                                                <div class="form-group">
-                                                    <label>Nama Perusahaan</label>
-                                                    <input type="text" class="text-left form-control" >
-                                                </div>
-                                            </b-col>
-                                            <b-col lg="6">
-                                                <div class="form-group">
-                                                    <label>Nama Perusahaan</label>
-                                                    <input type="text" class="text-right form-control" >
-                                                </div>
-                                            </b-col>
-                                        </b-row>
-                                        <div class="form-group description">
-                                            <label >Deskripsi Singkat</label>
-                                            <input type="text" class="form-control" >
-                                        </div>
-                                        <div class="button-experience my-5 text-center">
-                                            <button class="btn btn-add">
-                                                <p class="pt-3">Tambah Pengalaman Kerja</p>
-                                            </button>
-                                        </div>
-                                    </form>
-                                </div>
-                                </b-col>
-                            </b-row>
-                        </b-col>
-                        <!-- portfolio -->
-                        <b-col lg="12" class="my-5 px-4">
-                            <b-row class="portfolio">
-                                <h1 class="px-5 pt-4" style="font-size:25px;">Portofolio</h1>
-                                <span>
-                                    <img src="../assets/img/Line 10.svg" alt="">
-                                </span>
-                                <b-col lg="12">
-                                <div class="px-5 py-2">
-                                    <form>
-                                        <div class="form-group">
-                                            <label>Nama Aplikasi</label>
-                                            <input type="text" class="form-control" >
-                                        </div>
-                                        <div class="form-group">
-                                            <label>Link Respository</label>
-                                            <input type="text" class="form-control" >
-                                        </div>
-                                        <b-col lg="12">
-                                        <div class="form-group">
-                                            <label>Type Portofolio</label>
-                                           <b-row>
-                                               <b-col lg="6">
-                                                <div class="form-check">
-                                                <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
-                                                <label class="form-check-label" for="exampleRadios1">
-                                                    Aplikasi Android
-                                                </label>
-                                                </div>
-                                                </b-col>
-                                                <b-col lg="6">
-                                                <div class="form-check">
-                                                <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
-                                                <label class="form-check-label" for="exampleRadios1">
-                                                    Aplikasi Android
-                                                </label>
-                                                </div>
-                                                </b-col>
-                                            </b-row>
-                                            <div class="my-4">
-                                                <input type="file">
-                                            </div>
-
-                                        </div>
-                                        </b-col>
-                                        <div class="button-experience my-5 text-center">
-                                            <button class="btn btn-add">
-                                                <p class="pt-3">Tambah Portofolio</p>
-                                            </button>
-                                        </div>
-                                    </form>
-                                </div>
-                                </b-col>
-                            </b-row>
-                        </b-col>
-                        </b-row>
+                    <b-col class="edit-profile" lg="8" cols="12">
+                        <EditProfile />
                     </b-col>
                 </b-row>
             </b-col>
       </div>
+      <div>
+          <Footer />
+      </div>
 </div>
-
-    <!-- </div> -->
 </template>
 <script>
 import UserCard from '../components/UserCard'
 import Navbar from '../components/Navbar'
+import EditProfile from '../components/EditProfile'
+import Footer from '../components/Footer'
 export default {
+  data () {
+    return {
+      role: 0
+    }
+  },
   components: {
     UserCard,
-    Navbar
+    Navbar,
+    EditProfile,
+    Footer
   }
 }
 </script>
@@ -186,41 +46,16 @@ export default {
         background: #5E50A1;
         height: 200px;
     }
-    .profile-user{
-        background: #FFFFFF;
-border-radius: 8px;
+    .user-card{
+        height: 600px;
     }
-    /* .line{
-        border-bottom: 23px solid #C4C4C4;
+    /* .profile{
+        padding: 0px 5px;
+        margin-top: 5px;
     } */
-    .skill-profile{
-        background: #FFFFFF;
-border-radius: 8px;
-    }
-    .experience{
-        background: #FFFFFF;
-border-radius: 8px;
-    }
-    .portfolio{
-        background: #FFFFFF;
-border-radius: 8px;
-    }
-    .button-experience{
-        border: 1px solid #FBB017;
-box-sizing: border-box;
-border-radius: 4px;
-    }
-    .btn-save{
-        background: #FBB017;
-border-radius: 4px;
-color: white;
-    }
-    .btn-save:hover{
-        background: white;
-        border-radius: 4px;
-        color: #FBB017;
-    }
-    .description input{
-        height: 144px;
-    }
+    @media only screen and (max-width: 600px) {
+  .edit-profile {
+    margin-top: 25px;
+  }
+}
 </style>
